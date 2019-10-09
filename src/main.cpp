64c71a89804e9b2bc39fb08e3c9855d5a5a9f426
CoderSG <rob@robmaye.codes>
2019-10-09 22:00:26 +0800

    Added a input for the Reset pin, so we can see when the 6502 is being reset.

--- src/main.cpp
+++ src/main.cpp
@@ -5,56 +5,77 @@ const uint8_t ADDRESS_INPUTS[16] = {23,25,27,29,31,33,35,37,39,41,43,45,47,49,51
 
 // the 8 data pins
 const uint8_t DATA_INPUTS[8] = {22,24,26,28,30,32,34,38};
-// The input clock signal.
+
+// Clock signalinput pin.
 #define CLOCK_INPUT 19
+
+// Read/Write input pin
 #define READWRITE_INPUT 18
+
+// Reset input pin
+#define RESET_INPUT 17
 
 // The Values
 uint8_t ADDRESS_VALUES[16];
 uint8_t DATA_VALUES[8];
 uint8_t READWRITE_VALUE;
+uint8_t RESET_VALUE;
 
-// "I'm Alive!" LED Indicator blink time
+// "I'm Alive!" LED Indicator blink time (in milliseconds)
 #define ImAliveLedBlinkSpeed 1000
 
-// Reads the values of the A0-A15 pins (the Address Bus), D0-D7 (Data Bus) and RWB (Read/Write for the Data Bus)
+// Reads the values of the A0-A15 pins (the Address Bus), D0-D7 (Data Bus), RWB (Read/Write for the Data Bus) and RST (Reset)
 void measure()
 {
-  // Read all the values
-  for (byte i = 0; i < 16; ++i)
-  {
-    ADDRESS_VALUES[i] = digitalRead(ADDRESS_INPUTS[i]);
-  } 
-  for (byte i = 0; i < 8; ++i)
-  {
-    DATA_VALUES[i] = digitalRead(ADDRESS_INPUTS[i]);
-  }
-  READWRITE_VALUE = digitalRead(READWRITE_INPUT);
 
+  RESET_VALUE = digitalRead(RESET_INPUT);
+
+  // If we are resetting (RESET is LOW) then no point in reading the values.
+  if (RESET_VALUE == 1)
+  {
+    // Read all the values
+    for (byte i = 0; i < 16; ++i)
+    {
+      ADDRESS_VALUES[i] = digitalRead(ADDRESS_INPUTS[i]);
+    } 
+    for (byte i = 0; i < 8; ++i)
+    {
+      DATA_VALUES[i] = digitalRead(ADDRESS_INPUTS[i]);
+    }
+    READWRITE_VALUE = digitalRead(READWRITE_INPUT);
+  }
 }
 
 // Sends the values of the Address Bus, Data Bus and Read/Write stata to the Serial connection.
 void outputMeasurements()
 {
-  uint16_t addressValue = 0;
-  uint8_t dataValue = 0;
-
-  // Calculate the 16-bit unsigned int value from the Address Bus values
-  for (byte i = 0; i < 16; i++)
+  // RESET value of LOW (0) means the 6502 is being reset
+  if (RESET_VALUE == 0)
   {
-    addressValue |= (ADDRESS_VALUES[i] << i);
+    Serial.println("--RESET--");
   }
-
-  // Calculate the 8-bit unsigned int value from the Data Bus values
-  for (byte i = 0; i < 8; i++)
+  else
   {
-    dataValue |= (DATA_VALUES[i] << i);
-  }
+    uint16_t addressValue = 0;
+    uint8_t dataValue = 0;
 
-  // Send a formatted string via the Serial connection.
-  char output[20];
-  sprintf(output, "[%c] ADR: %04x  DATA: %02x (%u)", (READWRITE_VALUE)? 'R' : 'W', addressValue, dataValue, dataValue);
-  Serial.println(output);
+    // Calculate the 16-bit unsigned int value from the Address Bus values
+    for (byte i = 0; i < 16; i++)
+    {
+      addressValue |= (ADDRESS_VALUES[i] << i);
+    }
+
+    // Calculate the 8-bit unsigned int value from the Data Bus values
+    for (byte i = 0; i < 8; i++)
+    {
+      dataValue |= (DATA_VALUES[i] << i);
+    }
+
+    // Send a formatted string via the Serial connection.
+    char output[20];
+    sprintf(output, "[%c] ADR: %04x  DATA: %02x (%u)", (READWRITE_VALUE)? 'R' : 'W', addressValue, dataValue, dataValue);
+    Serial.println(output);
+  }
 }
 
 // Measures the outputs and sends a summary via the serial connection.
